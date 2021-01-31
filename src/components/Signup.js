b04fae461510b419e840a7f64470387216bc6935
Chris Athanas <chris@crowdskout.com>
2021-01-30 19:57:37 -0600

    error handling on submit user/email

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,17 +1,41 @@
-import React, { useRef } from 'react';
-import { Form, Button, Card } from 'react-bootstrap';
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext'
 
 export default function Signup() {
   const emailRef = useRef()
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
+  const { signup } = useAuth()
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+      return setError('Passwords do not match')
+    }
+    
+    console.log(emailRef.value)
+    try {
+      setError("")
+      setLoading(true)
+      await signup(emailRef.current.value, passwordRef.current.value)
+    } catch {
+      setError('Failed to create an account') 
+    }
+
+    setLoading(false)
+  }
 
   return (
     <>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-2">Sign Up</h2>
-            <Form>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <Form onSubmit={handleSubmit}>
               <Form.Group id='email'>
                 <Form.Label>Email</Form.Label>
                 <Form.Control type="email" ref={emailRef} required />
@@ -24,14 +48,15 @@ export default function Signup() {
                 <Form.Label>Confirm Password</Form.Label>
                 <Form.Control type="password" ref={passwordConfirmRef} required />
               </Form.Group>
-              <div class="d-flex justify-content-end">
-              <Button className="btn-lg" type="submit">Sign Up</Button>
+              <div className="d-flex justify-content-end">
+              <Button disabled={loading} className="btn-lg" type="submit">Sign Up</Button>
               </div>
             </Form>
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-       Already have an account? <a href="http://cnn.com">Log in</a>
+       {/* Already have an account? <Link to="/login">Log in</Link> */}
+       Already have an account? Log in
       </div> 
     </>
   )
