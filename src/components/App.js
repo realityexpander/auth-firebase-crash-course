a5b1d4c6bcdf48c78611692058886116868b0b9f
Chris Athanas <chris@crowdskout.com>
2021-01-30 22:26:14 -0600

    finish sign up

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Signup from './Signup';
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from '../contexts/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
