7a1051c9d1f1fad670167b6c25e6f0ca9b0f1753
Chris Athanas <chris@crowdskout.com>
2021-01-30 18:07:08 -0600

    add container to signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -31,7 +31,7 @@ export default function Signup() {
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2">
-       Already have an account?
+       Already have an account? <a href="http://cnn.com">Log in</a>
       </div> 
     </>
   )
