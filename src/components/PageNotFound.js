94722248903b465205faa55cb6f94817afd7fc36
Chris Athanas <chris@crowdskout.com>
2021-01-31 18:07:43 -0600

    Add code to pageNotFound component

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -5,7 +5,8 @@ export default function PageNotFound({ location }) {
  
  return (
   <>
-    Page not found - the path "{location && location.pathname}" did not match
+    Page not found - the path 
+    <span><code> "{location && location.pathname}" </code></span> did not match
     any Router routes.
   </>
  )
